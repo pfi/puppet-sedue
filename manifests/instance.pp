c96768e424301505de85aa08fdec8e8e9595bcdb
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2011-06-09 16:45:57 +0900

    class instance became a parameterized test

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -1,24 +1,24 @@
 # sedue_home is provided as a custom fact.
 
-class sedue::instance {
+class sedue::instance($instance_name) {
   file { 'serv_directory':
-    path => "${sedue_home}/etc/serve/puppet_test",
+    path => "${sedue_home}/etc/serve/${instance_name}",
     ensure => directory,
     owner => $user,
     group => $user,
     mode => '0755'
   }
-  # TODO: generate serve-supervise_#{instance} symlink
+  # TODO: generate serve-supervise_#{instance_name} symlink
 
   class { 'sedue::searcher':
     user => 'nobu',
-    instance => 'puppet_test',
+    instance => $instance_name,
     config_servers => 'bull0:25999'
   }
 
   class { 'sedue::indexer':
     user => 'nobu',
-    instance => 'puppet_test',
+    instance => $instance_name,
     config_servers => 'bull0:25999'
   }
 }
