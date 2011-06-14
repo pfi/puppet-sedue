9a30351f94e3b74002387dee5204fe4abd19cb2d
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2011-06-14 15:34:01 +0900

    user and config_servers parameter became arguments

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -1,22 +1,22 @@
 # sedue_home is provided as a custom fact.
 
-define sedue::instance($instance) {
+define sedue::instance($user, $instance, $config_servers) {
   sedue::serve_supervise { "sedue::${instance}::serve_supervise":
-    user => 'nobu',
+    user => $user,
     instance => $instance
   }
 
   sedue::server_script { "sedue::${instance}::searcher_script":
-    user => 'nobu',
+    user => $user,
     instance => $instance,
-    config_servers => 'bull0:25999',
+    config_servers => $config_servers,
     server_type => 'searcher'
   }
 
   sedue::server_script { "sedue::${instance}::indexer_script":
-    user => 'nobu',
+    user => $user,
     instance => $instance,
-    config_servers => 'bull0:25999',
+    config_servers => $config_servers,
     server_type => 'indexer'
   }
 
