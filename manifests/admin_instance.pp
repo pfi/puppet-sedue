5d488c56af325b663cd807caa66feb1c0cc18659
Masahiro Nakagawa <nakagawa@preferred.jp>
2011-06-21 18:58:10 +0900

    Apply #4520 changes. sedue::admin_instance create $SEDUE_HOME/etc/admin.instance. fixes #4530

--- manifests/admin_instance.pp
+++ manifests/admin_instance.pp
@@ -1,4 +1,4 @@
-define sedue::admin_instance($user, $instance, $config_server) {
+define sedue::admin_instance($user, $instance, $instance_file_content, $config_server) {
   sedue::directory { "sedue::${instance}::admin::directory_log":
     user => $user,
     instance => $instance,
@@ -11,11 +11,21 @@ define sedue::admin_instance($user, $instance, $config_server) {
     require => Sedue::Directory["sedue::${instance}::admin::directory_log"]
   }
 
+  file { "sedue::${instance}::admin::instance_file":
+    path => "${sedue_home}/etc/${instance}.instance",
+    owner => $user,
+    group => $user,
+    mode => '0644',
+    content => $instance_file_content,
+    require => File["${instance}_serve_directory"]
+  }
+
   sedue::mongodb { "sedue::${instance}::admin::mongodb":
     user => $user,
     instance => $instance,
     port => $config_server['port'],
     pair => $config_server['pair'],
     run => $config_server['run'],
+    require => File["sedue::${instance}::admin::instance_file"]
   }
 }
