a0e7000b04958151141f1ac8fae9e1e781ebb8db
Masahiro Nakagawa <nakagawa@preferred.jp>
2011-07-29 15:39:28 +0900

    Remove the removal of scripts routine from mongodb.pp. fixes #4679

--- manifests/mongodb.pp
+++ manifests/mongodb.pp
@@ -1,46 +1,28 @@
 # sedue::mongodb assumes sedue::supervise already called
 
 define sedue::mongodb($user, $instance, $port, $pair, $run) {
-  if $run {
-    file { "sedue::${instance}::mongodb::directory_repos":
-      path => "${sedue_home}/repos/${instance}",
-      owner => $user,
-      group => $user,
-      mode => '0755',
-      ensure => 'directory'
-    }
+  file { "sedue::${instance}::mongodb::directory_repos":
+    path => "${sedue_home}/repos/${instance}",
+    owner => $user,
+    group => $user,
+    mode => '0755',
+    ensure => 'directory'
+  }
 
-    # generate script and then run it
-    sedue::mongodb_script { "sedue::${instance}::mongodb::script":
-      user => $user,
-      instance => $instance,
-      port => $port,
-      pair => $pair,
-      run => $run,
-      require => File["sedue::${instance}::mongodb::directory_repos"]
-    }
+  # generate script and then run it
+  sedue::mongodb_script { "sedue::${instance}::mongodb::script":
+    user => $user,
+    instance => $instance,
+    port => $port,
+    pair => $pair,
+    run => $run,
+    require => File["sedue::${instance}::mongodb::directory_repos"]
+  }
 
-    sedue::server_process { "sedue::${instance}::mongodb::process":
-      instance => $instance,
-      server_type => 'mongodb',
-      run => $run,
-      require => Sedue::Mongodb_script["sedue::${instance}::mongodb::script"]
-    }
-  } else {
-    # stop process and then delete script
-    sedue::server_process { "sedue::${instance}::mongodb::process":
-      instance => $instance,
-      server_type => 'mongodb',
-      run => $run
-    }
-
-    sedue::mongodb_script { "sedue::${instance}::mongodb::script":
-      user => $user,
-      instance => $instance,
-      port => $port,
-      pair => $pair,
-      run => $run,
-      require => Sedue::Server_process["sedue::${instance}::mongodb::process"]
-    }
+  sedue::server_process { "sedue::${instance}::mongodb::process":
+    instance => $instance,
+    server_type => 'mongodb',
+    run => $run,
+    require => Sedue::Mongodb_script["sedue::${instance}::mongodb::script"]
   }
 }
